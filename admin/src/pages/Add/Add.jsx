9ca38b5b87609f3533b98d3c4b30087cde824d83
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-15 09:03:01 +0530

    API call success

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,8 +1,10 @@
 import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios";
 
 const Add = () => {
+  const url = "http://localhost:3000";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -15,7 +17,7 @@ const Add = () => {
     const value = event.target.value;
     setData((prevData) => ({ ...prevData, [name]: value }));
   };
-  const onSubmitHandler = (event) => {
+  const onSubmitHandler = async (event) => {
     event.preventDefault();
     const formData = new FormData();
     formData.append("name", data.name);
@@ -23,6 +25,17 @@ const Add = () => {
     formData.append("price", Number(data.price));
     formData.append("category", data.category);
     formData.append("image", image);
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+    } else {
+    }
   };
 
   return (
