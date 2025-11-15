a455e9f701b18d8a07c9766e5ca91e367b49a274
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-15 10:17:00 +0530

    feat: update List component to display food items with details

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -9,7 +9,7 @@ const List = () => {
   const [list, setList] = useState([]);
   const fetchList = async () => {
     const response = await axios.get(`${url}/api/food/list`);
-    console.log(response.data)
+    console.log(response.data);
     if (response.data.success) {
       setList(response.data.data);
     } else {
@@ -19,7 +19,29 @@ const List = () => {
   useEffect(() => {
     fetchList();
   }, []);
-  return <div>List</div>;
+  return (
+    <div className="list add flex-col">
+      <p>All Doods List</p>
+      <div className="list-table">
+        <b>Image</b>
+        <b>Name</b>
+        <b>Category</b>
+        <b>Price</b>
+        <b>Action</b>
+      </div>
+      {list.map((item, index) => {
+        return (
+          <div key={index} className="list-table-format">
+            <img src={`${url}/images/` + item.image} alt="" />
+            <p>{item.name}</p>
+            <p>{item.category}</p>
+            <p>${item.price}</p>
+            <p>X</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 };
 
 export default List;
