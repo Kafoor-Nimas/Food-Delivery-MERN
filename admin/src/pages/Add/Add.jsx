9a13ea7fe85889e39ae895a81e481db284679f99
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-15 08:26:43 +0530

    feat: implement form handling and image upload functionality in Add page

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,25 +1,55 @@
-import React from "react";
+import React, {  useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
 
 const Add = () => {
+  const [image, setImage] = useState(false);
+  const [data, setData] = useState({
+    name: "",
+    description: "",
+    price: "",
+    category: "Salad",
+  });
+  const onChangeHadldler = (event) => {
+    const name = event.target.name;
+    const value = event.target.value;
+    setData((prevData) => ({ ...prevData, [name]: value }));
+  };
+
   return (
     <div className="add">
       <form className="flex-col">
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
-            <img src={assets.upload_area} alt="" />
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
           </label>
-          <input type="file" id="image" hidden required />
+          <input
+            onChange={(event) => setImage(event.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
         </div>
         <div className="add-product-name flex-col">
           <p>Product name</p>
-          <input type="text" name="name" placeholder="Type here" />
+          <input
+            onChange={onChangeHadldler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
         </div>
         <div className="add-product-description flex-col">
           <p>Product description</p>
           <textarea
+            onChange={onChangeHadldler}
+            value={data.description}
             name="description"
             rows="6"
             placeholder="Write content here"
@@ -29,7 +59,7 @@ const Add = () => {
         <div className="add-category-price">
           <div className="add-category flex-col">
             <p>Product category</p>
-            <select name="category">
+            <select name="category" onChange={onChangeHadldler}>
               <option value="Salad">Salad</option>
               <option value="Rolls">Rolls</option>
               <option value="Deserts">Deserts</option>
@@ -42,7 +72,13 @@ const Add = () => {
           </div>
           <div className="add-price flex-col">
             <p>Product price</p>
-            <input type="Number" name="price" placeholder="$20" />
+            <input
+              type="Number"
+              name="price"
+              placeholder="$20"
+              onChange={onChangeHadldler}
+              value={data.price}
+            />
           </div>
         </div>
         <button type="submit" className="add-btn">
