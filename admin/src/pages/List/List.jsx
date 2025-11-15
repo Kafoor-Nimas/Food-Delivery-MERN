5a427be71834eb1d3da6bc22cddd8e8ab1325a82
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-15 10:00:29 +0530

    feat: implement data fetching and error handling in List component

--- admin/src/pages/List/List.jsx
+++ admin/src/pages/List/List.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
-import './List.css'
+import React, { useEffect } from "react";
+import "./List.css";
+import { useState } from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
 
 const List = () => {
-  return (
-    <div>List</div>
-  )
-}
+  const url = "http://localhost:3000";
+  const [list, setList] = useState([]);
+  const fetchList = async () => {
+    const response = await axios.get(`${url}/api/food/list`);
+    console.log(response.data)
+    if (response.data.success) {
+      setList(response.data.data);
+    } else {
+      toast.error("Error");
+    }
+  };
+  useEffect(() => {
+    fetchList();
+  }, []);
+  return <div>List</div>;
+};
 
-export default List
+export default List;
