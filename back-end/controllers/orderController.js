dc45f6f9d5f9860a896572f11152790fbb470600
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-16 14:38:17 +0530

    place order initial back-end setup

---
+++ back-end/controllers/orderController.js
@@ -0,0 +1,7 @@
+import orderModel from "../models/OrderModel.js";
+import userModel from "../models/userModel.js";
+import stripe from "stripe";
+
+//Place user order for front-end
+const placeOrder = async (req, res) => {};
+export { placeOrder };
