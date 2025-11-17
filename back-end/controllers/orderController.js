ba8a584ccfddf85a6cca2077951d7ea51a693d6f
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-17 17:40:22 +0530

    display order back-end api setup finish

--- back-end/controllers/orderController.js
+++ back-end/controllers/orderController.js
@@ -83,5 +83,16 @@ const userOrders = async (req, res) => {
     res.json({ success: false, message: "Error" });
   }
 };
+//Listing orders for admin pannel
 
-export { placeOrder, verifyOrder, userOrders };
+const listOrders = async (req, res) => {
+  try {
+    const orders = await orderModel.find({});
+    res.json({ success: true, data: orders });
+  } catch (error) {
+    console.log(error);
+    res.json({ success: false, message: "Error" });
+  }
+};
+
+export { placeOrder, verifyOrder, userOrders, listOrders };
