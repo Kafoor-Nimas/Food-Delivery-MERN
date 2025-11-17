0f015495059928310c75f917e0694b94be0ade1c
Kafoor-Nimas <nimask-ct21004@stu.kln.ac.lk>
2025-11-17 07:09:53 +0530

    integrate place order back-end with front-end

--- front-end/src/pages/PlaceOrder/PlaceOrder.jsx
+++ front-end/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,9 +1,22 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../context/StoreContext";
 
 const PlaceOrder = () => {
-  const { getTotalCartAmount } = useContext(StoreContext);
+  const { getTotalCartAmount, token, foodList, cartItems, url } =
+    useContext(StoreContext);
+
+  const [data, setData] = useState({
+    firstName: "",
+    lastName: "",
+    email: "",
+    street: "",
+    city: "",
+    state: "",
+    zipcode: "",
+    country: "",
+    phone: "",
+  });
   return (
     <form className="place-order">
       <div className="place-order-left">
